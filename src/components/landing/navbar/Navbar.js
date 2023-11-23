031f549da778052ef01bcf5680a28c5fa4aab0bd
joelnyongesa <joelnyongesa148@gmail.com>
2023-11-23 15:57:17 +0300

    refactor: minor fixes and introduction of buttons on the navbar

--- src/components/landing/navbar/Navbar.js
+++ src/components/landing/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import logo from './logo.webp'
+import logo from './logo.webp';
 
 function Navbar() {
   return (
@@ -6,19 +6,37 @@ function Navbar() {
       <div className="w-1/12">
         <img src={logo} className="w-20 h-20 mr-5" alt=''/>
       </div>
-      <nav className=' w-full flex flex-row'>
+      <nav className='w-full flex flex-wrap justify-between'>
         <div className='w-1/2 py-4 flex flex-row justify-between px-5'>
-          {/* <FaLightbulb className="text-5xl pb-3" /> */}
-          
-          <div>About Us</div>
-          <div>Reviews</div>
-          <div>Contact Us</div>
-          <div>Sign Up</div>
+          <div>
+            <button className='bg-blue-100 p-2 hover:bg-blue-500 hover:text-white hover:cursor-pointer rounded-lg'>Products</button>
+          </div>
+          <div>
+            <button className='bg-blue-100 p-2 hover:bg-blue-500 hover:text-white hover:cursor-pointer rounded-lg'>About Us</button>
+          </div>
+          <div>
+            <button className='bg-blue-100 p-2 hover:bg-blue-500 hover:text-white hover:cursor-pointer rounded-lg'>Contact Us</button>
+          </div>
         </div>
-        <div className='w-1/2 p-5'>Search bar</div>
+        <div className='w-1/2 flex items-center justify-end p-5'>
+          <div className='flex'>
+            <input
+              className="appearance-none bg-transparent border-b-2 w-full mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="email"
+              placeholder="example@email.com"
+              aria-label="Email address"
+            />
+            <button
+              className="flex-shrink-0 bg-transparent hover:bg-transparent hover:text-black border text-sm text-black py-1 px-2 rounded"
+              type="button"
+            >
+              Search Item
+            </button>
+          </div>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
