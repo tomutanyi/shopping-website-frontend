48d2075eb3524267ed00554c8a6d37820dde2293
Njokimary <marynjoki738@gmail.com>
2023-11-24 13:09:28 +0300

    feat:add signup to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from './About/About';
+import SignUp from './Signup/SignUp';
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen">
       <About />
+      <SignUp/>
     </div>
   );
 }
