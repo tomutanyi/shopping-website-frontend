e509bb353d6ea092d546ec4df844eb3934a88ef5
Njokimary <marynjoki738@gmail.com>
2023-11-24 10:27:07 +0300

    feat:add the form input fileds

--- src/components/Signup/SignUp.js
+++ src/components/Signup/SignUp.js
@@ -20,7 +20,64 @@ const SignUp = () => {
   };
 
   return (
-    
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-96 p-8 bg-white shadow-md rounded-md">
+        <h2 className="text-3xl font-bold mb-6">Sign Up</h2>
+
+        {/* Sign Up Form */}
+        <form onSubmit={handleSubmit}>
+          {/* Name Field */}
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-600">Name</label>
+            <input
+              type="text"
+              id="name"
+              className="w-full p-2 border border-gray-300 rounded-md"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+
+          {/* Email Field */}
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-600">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="w-full p-2 border border-gray-300 rounded-md"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          {/* Password Field */}
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-600">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="w-full p-2 border border-gray-300 rounded-md"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
