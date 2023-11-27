b3058342200c0e1443ae1f3db2b68796a2da0bb7
joelnyongesa <joelnyongesa148@gmail.com>
2023-11-27 14:33:12 +0300

    refactor: mino UI footer fixes

--- src/components/landing/footer/Footer.js
+++ src/components/landing/footer/Footer.js
@@ -5,12 +5,12 @@ function Footer() {
     const currentYear = new Date().getFullYear();
   return (
     <div>
-        <footer className='bg-blue-500 text-white px-10 py-5 flex flex-col'>
+        <footer className='bg-blue-500 rounded-lg text-white px-10 py-5 flex flex-col'>
             <div className='flex flex-row justify-evenly'>
                 <div className='flex flex-col'>
                     <h1 className='mb-2 font-bold'>Subscribe to our Newsletter</h1>
                     <div className='flex'>
-                        <input class="appearance-none bg-transparent border-b-2 w-full  mr-3 py-1 px-2 leading-tight focus:outline-none" type="email" placeholder="example@email.com" aria-label="Email address"/>
+                        <input class="appearance-none bg-transparent border-b-2 w-2/3  mr-3 py-1 px-2 leading-tight focus:outline-none" type="email" placeholder="example@email.com" aria-label="Email address"/>
                         <button class="flex-shrink-0 bg-transparent hover:bg-white hover:text-blue-500 border text-sm text-white py-1 px-2 rounded" type="button">
                         Subscribe
                         </button>
