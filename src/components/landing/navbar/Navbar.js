52ed594b9b416d77e38ddf46379fcd6908b2d07b
joelnyongesa <joelnyongesa148@gmail.com>
2023-11-30 14:53:52 +0300

    feat: log in and out buttons

--- src/components/landing/navbar/Navbar.js
+++ src/components/landing/navbar/Navbar.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ user, onLogout }) {
   return (
     <div className="bg-blue-500 text-white p-5 rounded-lg">
-      <nav className='w-full flex flex-wrap justify-between'>
-        <div className='w-1/2'></div>
-        <div className='w-1/2 py-4 flex flex-row justify-between px-5'>
-          <div>
-            <Link to="/" className='bg-none p-2 hover:bg-blue-700 hover:text-white hover:cursor-pointer rounded-lg'>Home</Link>
-          </div>
-          <div>
-            <Link to="/about" className='bg-none p-2 hover:bg-blue-700 hover:text-white hover:cursor-pointer rounded-lg'>About Us</Link>
-          </div>
-          <div>
-            <Link to="/signup" className='bg-none p-2 hover:bg-blue-700 hover:text-white hover:cursor-pointer rounded-lg'>Sign Up</Link>
-          </div>
-          <div>
-            <Link to="/login" className='bg-none p-2 hover:bg-blue-700 hover:text-white hover:cursor-pointer rounded-lg'>Login</Link>
-          </div>
-          <div>
-            <Link to="/Products" className='bg-none p-2 hover:bg-blue-700 hover:text-white hover:cursor-pointer rounded-lg'>Products</Link>
-          </div>
+      <nav className="w-full flex flex-wrap justify-between">
+        <div className="w-1/2 py-4 flex flex-row justify-between px-5">
+          {user ? (
+            <div>
+              <p>Welcome, {user.username}</p>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
-        {/* <div className='w-1/2 flex items-center justify-end p-5'>
-          <div className='flex'>
-            <input
-              className="appearance-none bg-transparent border-b-2 w-full mr-3 py-1 px-2 leading-tight focus:outline-none text-white"
-              type="email"
-              placeholder="example@email.com"
-              aria-label="Email address"
-            />
-            <button
-              className="flex-shrink-0 bg-transparent hover:bg-white hover:text-blue-500 border text-sm text-white py-1 px-2 rounded"
-              type="button"
-            >
-              Search Item
+        <div className="w-1/2 py-4 flex flex-row justify-between px-5">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About Us</NavLink>
+          <NavLink to="/products">Products</NavLink>
+          {user ? (
+            <button className="px-2 bg-none hover:bg-blue-700 hover:text-white hover:cursor-pointer rounded-lg" onClick={onLogout}>
+              Logout
             </button>
-          </div>
-        </div> */}
+          ) : (
+            <div className="flex flex-row justify-end">
+              <NavLink to="/signup">Sign Up</NavLink>
+              <NavLink to="/login">Login</NavLink>
+            </div>
+          )}
+        </div>
       </nav>
     </div>
   );
 }
 
-export default Navbar;
+function NavLink({ to, children }) {
+  return (
+    <div>
+      <Link
+        to={to}
+        className="bg-none p-2 hover:bg-blue-700 hover:text-white hover:cursor-pointer rounded-lg"
+      >
+        {children}
+      </Link>
+    </div>
+  );
+}
 
+export default Navbar;
