054c052515ce64f6ad39387dafc5cc1a57a2f372
joelnyongesa <joelnyongesa148@gmail.com>
2023-11-30 14:53:02 +0300

    passed down props from App to Navbar

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -2,10 +2,10 @@ import {Outlet } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
 import Footer from "./footer/Footer";
 
-function Landing() {
+function Landing({ user, onLogout}) {
   return (
     <div>
-        <Navbar />
+        <Navbar user={user} onLogout={onLogout} />
         <Outlet></Outlet>
         <Footer />
     </div>
