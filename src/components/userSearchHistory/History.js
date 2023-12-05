d5472f3e928662695ed972af0536adb48bce5577
Iqra Ali <iqra.ali@student.moringaschool.com>
2023-12-05 09:04:48 +0300

    header-history

--- src/components/userSearchHistory/History.js
+++ src/components/userSearchHistory/History.js
@@ -1,21 +1,26 @@
+
 import React from 'react';
 import { motion } from 'framer-motion';
 
 const History = ({ historyItem, onClick }) => {
-
   if (!historyItem) {
-    return null; 
+    return null;
   }
 
   return (
-    <motion.div
-      className="history-card"
-      whileHover={{ borderColor: '#808080' }}
-      onClick={() => onClick(historyItem.pageUrl)}
-    >
-      <p>{historyItem.pageUrl}</p>
-      <p className="date-time" style={{ color: 'green' }}>{historyItem.dateTime}</p>
-    </motion.div>
+    <div>
+      <h1 style={{ textAlign: 'center', color: '#333' }}>My History</h1>
+      <motion.div
+        className="history-card"
+        whileHover={{ borderColor: '#808080' }}
+        onClick={() => onClick(historyItem.pageUrl)}
+      >
+        <p>{historyItem.pageUrl}</p>
+        <p className="date-time" style={{ color: 'green' }}>
+          {historyItem.dateTime}
+        </p>
+      </motion.div>
+    </div>
   );
 };
 
