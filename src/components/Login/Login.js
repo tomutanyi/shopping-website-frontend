e791a365951209d65d986209eb2dcc3cc3fc24e7
tomutanyi <tomutanyi@gmail.com>
2023-12-14 05:49:23 +0300

    commented out importing bcrypt

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import bcrypt from 'bcryptjs';
+// import bcrypt from 'bcryptjs';
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState('');
